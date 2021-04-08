b9fd4ef68da254e27de5d1438d8da101f2706659
DiazRock <corolariodiaz@gmail.com>
2021-04-08 13:40:01 -0400

    Added new views

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from platzigram import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi)
 ]
