4c92c3dbda3e5d86d3d2dcc8e8b503f507711b23
DiazRock <corolariodiaz@gmail.com>
2021-04-09 13:27:45 -0400

    Fixed the login view

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -19,11 +19,15 @@ from django.conf import settings
 from django.urls import path
 from platzigram import views as local_views
 from posts import views as post_views
+from users import views as users_views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello-world/', local_views.hello_world),
-    path('sorted/', local_views.sorted),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi),
-    path('posts/', post_views.list_posts),
+    path('admin/', admin.site.urls, ),
+    path('hello-world/', local_views.hello_world,name= 'hello_world'),
+    path('sorted/', local_views.sorted, name= 'sorted'),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi, name= 'hi'),
+    path('posts/', post_views.list_posts, name = 'feed'),
+    path('users/login/', users_views.login_view, name= 'login')
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
