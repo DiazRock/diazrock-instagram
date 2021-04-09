88ee28c8af78ca1116c528db2a03e5d429a1690d
DiazRock <corolariodiaz@gmail.com>
2021-04-09 11:39:07 -0400

    Modified user models

--- platzigram/users/models.py
+++ platzigram/users/models.py
@@ -1,32 +1,32 @@
-"""User models. """
+"""Users models."""
 
+# Django
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Profile(models.Model):
-    """ 
-    Profile model.
-    
-    Proxy model that extends the base data with other information 
-    """
-    
-    users = models.OneToOneField(User, on_delete= models.CASCADE)
+    """Profile model.
 
-    website = models.URLField(max_length= 200, blank= True)
-    biography = models.TextField(blank= True)
-    phone_number = models.CharField(max_length= 20, blank= True)
-    
+    Proxy model that extends the base data with other
+    information.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
     picture = models.ImageField(
-        upload_to= 'users/pictures',
-        blank= True,
-        null= True
+        upload_to='users/pictures',
+        blank=True,
+        null=True
     )
 
-    created = models.DateTimeField(auto_now_add= True)
-    modified = models.DateTimeField(auto_now= True)
-    
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        """ Return username """
-        
+        """Return username."""
         return self.user.username
