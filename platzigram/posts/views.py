f5fbdae00ea875d0bdb4c0686c9c2aa9c765e2b8
DiazRock <corolariodiaz@gmail.com>
2021-04-10 15:01:27 -0400

    Modified views for posts

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,9 +1,12 @@
 """ Posts views """
+
 from datetime import datetime
 #Django 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from posts.forms import PostForm
 
 # Create your views here.
 
@@ -37,6 +40,29 @@ posts = [
     }
 ]
 
+
+@login_required
+def create_post(request):
+    """ Create a new post view """
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+        
+    else:
+        form= PostForm()
+    
+    return render(
+        request= request,
+        template_name= 'posts/new.html',
+        context= {
+            'form': form,
+            'user': request.user,
+            'profile': request.user.profile
+        }
+    )
+        
 
 @login_required
 def list_posts(request):
