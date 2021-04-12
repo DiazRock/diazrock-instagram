d04445ea1b3318764c4395a9bfb1408d78e78756
DiazRock <corolariodiaz@gmail.com>
2021-04-12 10:43:29 -0400

    Posts views update to classes

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,48 +1,45 @@
-""" Posts views """
+"""Posts views."""
 
-from datetime import datetime
+# Django
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView, ListView
 
-# Django 
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+# Forms
+from posts.forms import PostForm
 
 # Models
 from posts.models import Post
 
 
-# Forms
-from posts.forms import PostForm
+class PostsFeedView(LoginRequiredMixin, ListView):
+    """Return all published posts."""
 
-# Create your views here.
+    template_name = 'posts/feed.html'
+    model = Post
+    ordering = ('-created',)
+    paginate_by = 30
+    context_object_name = 'posts'
 
-@login_required
-def create_post(request):
-    """ Create a new post view """
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            import pdb; pdb.set_trace()
-            return redirect('feed')
-        
-    else:
-        form= PostForm()
-    
-    return render(
-        request= request,
-        template_name= 'posts/new.html',
-        context= {
-            'form': form,
-            'user': request.user,
-            'profile': request.user.profile
-        }
-    )
-        
 
-@login_required
-def list_posts(request):
-    """ List posts """
-    posts = Post.objects.all().order_by('-created')
-    
-    return render(request, 'posts/feed.html', {'posts': posts})
+class PostDetailView(LoginRequiredMixin, DetailView):
+    """Return post detail."""
+
+    template_name = 'posts/detail.html'
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    """Create a new post."""
+
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+
+    def get_context_data(self, **kwargs):
+        """Add user and profile to context."""
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
