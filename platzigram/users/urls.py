b8de11d4736942a0dab1f628e1f9a1a48d9265ed
DiazRock <corolariodiaz@gmail.com>
2021-04-12 10:50:10 -0400

    Updated users urls

--- platzigram/users/urls.py
+++ platzigram/users/urls.py
@@ -10,28 +10,29 @@ from users import views
 
 urlpatterns = [
     
-    #Post
-    path(
-        route = '<str:username>/',
-        view= TemplateView.as_view(template_name = 'users/detail.html'),
-        name= 'detail'
-    ),
     
     # Management
     path(
-        route ='users/login/', 
-        view= views.login_view, 
+        route ='login/', 
+        view= views.LoginView.as_view(), 
         name= 'login'),
     path(
-        route= 'users/logout/', 
-        view= views.logout_view, 
+        route= 'logout/', 
+        view= views.LoginView.as_view(), 
         name= 'logout'),
     path(
-        route= 'users/signup/', 
-        view= views.signup, 
+        route= 'signup/', 
+        view= views.SignupView.as_view(), 
         name= 'signup'),
     path(
-        route='users/me/profile/', 
-        view= views.update_profile, 
+        route='me/profile/', 
+        view= views.UpdateProfileView.as_view(), 
         name='update_profile'),
+    
+    #Post
+    path(
+        route = '<str:username>/',
+        view= views.UserDetailView.as_view(),
+        name= 'detail'
+    )
 ]
