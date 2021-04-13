f84df69e7c69d6f4da8c7c7beb712b8369e7e987
DiazRock <corolariodiaz@gmail.com>
2021-04-12 20:46:10 -0400

    Posts count for profile model added

--- platzigram/users/models.py
+++ platzigram/users/models.py
@@ -3,6 +3,7 @@
 # Django
 from django.contrib.auth.models import User
 from django.db import models
+from posts.models import Post
 
 
 class FollowerRelation(models.Model):
@@ -23,7 +24,7 @@ class Profile(models.Model):
     website = models.URLField(max_length=200, blank=True)
     biography = models.TextField(blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
-    
+
     
     picture = models.ImageField(
         upload_to='users/pictures',
@@ -34,6 +35,12 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
+    @property
+    def posts_count(self):
+        """ Return posts count for profile """
+        return Post.objects.filter(user__username = self.user.username).count()
+        
+    
     def __str__(self):
         """Return username."""
         return self.user.username
