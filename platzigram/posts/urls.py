e7d02e062f7b7baaf5911792317660e0aef9383c
DiazRock <corolariodiaz@gmail.com>
2021-04-14 13:31:07 -0400

    Added followers feed

--- platzigram/posts/urls.py
+++ platzigram/posts/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
         route = '', 
         view = views.PostsFeedView.as_view(), 
         name = 'feed'),
-
+    path(
+        route = 'posts/followed_by_<str:username>', 
+        view = views.PostsFeedView.as_view(), 
+        name = 'feed_by_follow'),
     path(
         route ='posts/new/', 
         view = views.CreatePostView.as_view(), 
